8fc7d009258fddf67a6392a7f86d59d780b46104
Will Trimble <trimble@anl.gov>
2019-02-06 13:56:48 -0600

    Code formatting

--- autoskewer.py
+++ autoskewer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-import sys, os
+import sys
+import os
 from optparse import OptionParser
 
 try:
@@ -13,12 +14,12 @@ import shutil
 
 def idfiletype(fname):
     with open(fname) as p:
-       firstline = p.readline()
-       if firstline[0] == ">":
-           return "FASTA"
-       if firstline[0] == "@":
-           return "FASTQ"
-       sys.exit("Cannot identify sequence file type")
+        firstline = p.readline()
+        if firstline[0] == ">":
+            return "FASTA"
+        if firstline[0] == "@":
+            return "FASTQ"
+        sys.exit("Cannot identify sequence file type")
 
 def remove_fastx_suffix(fname):
     # remove suffixes .fastq and .fasta, retain other suffixes
@@ -67,14 +68,14 @@ def write_adapter_fasta(filename, namesandadapters):
 
 def read_fasta_to_table(filen):
     '''Loads a simple fasta file into a dictionary'''
-    table = {} 
+    table = {}
     try:
         FILE = open(filen)
     except IOError:
         sys.stderr.write("Can't open file "+ filen + "!\n")
         sys.exit(1)
-    for line in FILE: 
-        if line[0] == ">":  
+    for line in FILE:
+        if line[0] == ">":
             header = line.strip()[1:].split()[0]
         else:
             sequence = line.strip()
@@ -103,11 +104,11 @@ if __name__ == '__main__':
     parser.add_option("-l", "--logfile", dest="logfile", default=None, help="Output log file [default STDOUT]")
     parser.add_option("-t", "--tmpdir", dest="tmpdir", default=".", help="DIR for intermediate files [default CWD]")
     parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False, help="Verbose [default off]")
-  
+
     (opts, args) = parser.parse_args()
     if not (opts.input and os.path.isfile(opts.input)):
         if len(args) == 1 and os.path.isfile(args[0]):
-             filename = args[0]
+            filename = args[0]
         else:
             parser.error("Missing input file or wrong number of arguments")
     else:
@@ -119,13 +120,13 @@ if __name__ == '__main__':
     DATAPATH = os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), "data")
     PATHPREFIX = os.path.join(TMPDIR, os.path.basename(filestem))
     if opts.verbose:
-        print("TYPE: "+TYPE)
-        print("TMPDIR: "+TMPDIR)
-        print("DATAPATH: "+DATAPATH)
-        print("PATHPREFIX: "+PATHPREFIX)
+        print("TYPE:", TYPE)
+        print("TMPDIR:", TMPDIR)
+        print("DATAPATH:", DATAPATH)
+        print("PATHPREFIX:", PATHPREFIX)
     if not opts.output:  # guess output file name
         outputfile = filestem + ".scrubbed." + TYPE.lower()
-    else: 
+    else:
         outputfile = opts.output
     if not opts.logfile:
         if opts.output:
@@ -152,22 +153,24 @@ if __name__ == '__main__':
         print(P7adapter)
         print(P7r)
 
-    
+
     adaptorfile = PATHPREFIX + ".adapter.fa"
     skewoutname = PATHPREFIX + ".4"
     skewoptions = "-k 5 -l 0 --quiet -t {} -r .2 -m any".format(str(opts.processes))
-    write_adapter_fasta(adaptorfile, 
-                        [P5adaptername, P5adapter, P5adaptername+"_R", 
-                        P5r, P7adaptername, P7adapter, 
+    write_adapter_fasta(adaptorfile,
+                        [P5adaptername, P5adapter, P5adaptername+"_R",
+                        P5r, P7adaptername, P7adapter,
                         P7adaptername+"_R", P7r])
 
     skewcmd = "skewer -x {adaptorfile} {options} {filename} -o {filestem}.4".format(adaptorfile=adaptorfile, options=skewoptions, filename=filename, filestem=filestem)
     if opts.verbose:
         print(skewcmd)
     check_call(skewcmd.split(" "))
-    
+
     shutil.move(filestem + ".4-trimmed.fastq", outputfile)
     shutil.move(filestem + ".4-trimmed.log", logfile)
-    
+
     if not opts.verbose:
-        os.remove(PATHPREFIX+".P5.csv"); os.remove(PATHPREFIX+".P7.csv");
+        os.remove(adaptorfile)
+        os.remove(PATHPREFIX+".P5.csv")
+        os.remove(PATHPREFIX+".P7.csv")
